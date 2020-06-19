b7699206151178bde638f1c3f47aae5c058dbe66
Gabriel oliveira <gabriel@MacBook-Pro-de-Gabriel.local>
2020-06-19 11:33:38 -0300

    adding initial exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+def bounce_hits(number_of_times):
+    height = 100
+    counter = 1
+    bounced_proportion = 3/5
+    while counter <= number_of_times:
+        height *= bounced_proportion
+        print(round(height,4))
+        counter += 1
+
+bounce_hits(10)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,29 @@
 # mortgage.py
 #
 # Exercise 1.7
+def mortgage_calculator(principal, rate, payment, extra_payment_start_month, extra_payment_end_month, extra_payment):
+
+    months = 0
+    total_paid = 0.0
+
+    while principal > 0:
+        if months >= extra_payment_start_month and months <= extra_payment_end_month:
+            principal = principal * (1+rate/12) - (payment + extra_payment)
+            total_paid = total_paid + payment + extra_payment
+            months += 1
+        else:
+            principal = principal * (1+rate/12) - payment
+            total_paid = total_paid + payment
+            months += 1
+
+    print('Total paid', total_paid)
+    print('Total months', months)
+
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+
+mortgage_calculator(principal,rate, payment, extra_payment_start_month, extra_payment_end_month, extra_payment)
